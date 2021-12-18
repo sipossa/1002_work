c1412ee0180d8e44aac39e8fbdce1cd8e33ee57e
sipossa <sipossa@naver.com>
2021-12-18 14:13:44 +0900

    20211218

--- doc/scrollevent03.js
+++ doc/scrollevent03.js
@@ -1,11 +1,11 @@
 window.addEventListener('load', function(){
 
     const SEC = document.querySelectorAll('.ani');
-    const WT=window.innerHeight;
+    const WT = window.innerHeight;
 
     window.addEventListener("scroll", ()=>{
             let sct=window.scrollY;
-            SEC.forEach((e)=>{
+            SEC.forEach(e=>{
                 let secTop = e.offsetTop;
                 let secH=e.clientHeight;
                 sct > secTop -(WT-secH)/2 ? e.classList.add('on') : e.classList.remove('on');
