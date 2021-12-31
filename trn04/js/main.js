403ffa90cf31d10653f2d4387c6cddff63f017dd
sipossa09 <90168520+sipossa09@users.noreply.github.com>
2021-12-31 17:09:11 +0900

    20211231

--- trn04/js/main.js
+++ trn04/js/main.js
@@ -1,9 +1,23 @@
 $(function(){
 //------------------------------------------------------
+$('#popUp').draggable();
+
+$('#popUp a').on('click', function(){
+    $(this).parent().fadeOut();
+    return false;
+});
+
+$('.search').on('click', function(){
+    $('.searchForm').stop().slideToggle();
+})
+
 $(window).on('scroll', ()=>{
     let sct=$(window).scrollTop();
+    console.log(sTop,sct);
     sct > 0 ? $('#header').addClass('on') : $('#header').removeClass('on');
     //if(sct > 0) {$('#header').addClass('on')}else{$('#header').removeClass('on')}
+    sct > 300 ? $('#toTop').fadeIn() : $('#toTop').fadeOut();
+    sct > sTop ? $('#Solution').addClass('on') : $('#Solution').removeClass('on');
 });
 
 
@@ -41,6 +55,29 @@ $('.pr_slider figure').eq(4).addClass('on');
 $('.pr_slider').on('afterChange', function(e,s,c){
     $('.pr_slider figure').eq(c+4).addClass('on').siblings().removeClass('on');
 });
+
+
+$('.familyLink .link>a').on('click', function(){
+    //e.preventDefault();
+    $(this).prev().stop().slideToggle();
+    $(this).find('i').toggleClass('on');
+    return false;
+});
+
+// $(document).not('.familyLink .link>a').on('click', function(){
+//     $('.familyLink .link>a').prev().stop().slideUp();
+// });
+
+$('#toTop a').on('click', e=> {
+    e.preventDefault();
+    $('html,body').animate({scrollTop:0},1000)
+});
+
+let sTop = $('#Solution').offset().top - 200;
+
+//console.log(sTop)
+
+
 
 
 //------------------------------------------------------
